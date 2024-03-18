d03a6dbeecefadc939fa776a8dd9282c558e03a5
Le Nhut Tan <121606134+lenhuttanprograming@users.noreply.github.com>
2024-03-18 16:41:41 -0400

    Add toolbar and menu

--- app/src/main/java/ca/tanle/android_project_2/MainActivity.kt
+++ app/src/main/java/ca/tanle/android_project_2/MainActivity.kt
@@ -2,10 +2,50 @@ package ca.tanle.android_project_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection.
+        return when (item.itemId) {
+            R.id.home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.map -> {
+                Toast.makeText(this, "Map", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.places -> {
+                Toast.makeText(this, "Places", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.about -> {
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.share -> {
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
